81b839c4f34e453f4c5910e6119242752c30dac8
Paulo J. Oliveira <pauloj.oliveira.ext@e-ai.com.br>
2022-07-08 10:14:59 -0300

    Quinta tentativa de deploy do front

--- src/app/anumana.service.ts
+++ src/app/anumana.service.ts
@@ -11,7 +11,7 @@ export class AnumanaService
   constructor(private httpCliente: HttpClient) { }
 
   createAnumana(anumana: Anumana): Observable<Object> {
-    return this.httpCliente.post('https://hetucakra-api.herokuapp.com', anumana);
+    return this.httpCliente.post('https://hetucakra-api.herokuapp.com/', anumana);
   }
 
 }
